bd483f83ccb07520d076b540cc431a6131fa67a6
mirco mannucci <mirco@holomathics.com>
2025-07-31 14:19:46 -0400

    Delete src/evolution.py

--- src/evolution.py
+++
@@ -1,7 +0,0 @@
-from gtda.homology import VietorisRipsPersistence
-import numpy as np
-
-def compute_persistence_diagram(distance_matrix):
-    vr = VietorisRipsPersistence(metric='precomputed')
-    diagrams = vr.fit_transform([distance_matrix])
-    return diagrams[0]
